8c321db3082c6a23f562501ad6ff2f3828af3b1d
email = 1959132915@qq.com <1959132915@qq.com>
2020-12-17 14:19:15 +0800

    add alloc mode

--- core/alloc.py
+++ core/alloc.py
@@ -4,5 +4,8 @@ def alloc_virtualalloc():
 def alloc_malloc():
     return "(PCHAR)malloc(sizeof(offset_table));"
 
+def alloc_calloc():
+    return " (PCHAR)calloc(sizeof(offset_table), sizeof(char));"
+
 def alloc_maping():
     return "(PCHAR)MapViewOfFile(CreateFileMapping(INVALID_HANDLE_VALUE, NULL, PAGE_EXECUTE_READWRITE, 0, sizeof(offset_table), NULL), FILE_MAP_WRITE, 0, 0, sizeof(offset_table));"
