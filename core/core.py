19a0720c34081882360ff2a0e20e3cc62401860c
email = 1959132915@qq.com <1959132915@qq.com>
2021-01-12 10:30:34 +0800

    ...

--- core/core.py
+++ core/core.py
@@ -5,11 +5,7 @@ from core.alloc import *
 def generator(template="",shellcode_path="",seciton="",alloc="",encrypt="",output=""):
 
     shellcode = load_file(shellcode_path)
-    if encrypt=="offset":
-        tables_code = generate_offset(shellcode, seciton)
-    elif encrypt=="aes":
-        tables_code = generate_aes(shellcode, seciton);
-
+    tables_code = eval("generate_"+encrypt + "(shellcode, seciton)")
     module =__import__('templates.'+template)
     c_code = eval('module.'+template+'.template.get()')
     alloc_code = eval(alloc+"()")
